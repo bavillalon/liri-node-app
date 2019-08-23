88766e6ef1a659a6eb9d0010c269fe2856944f32
Bryan Villalon <bavillalon@gmail.com>
2019-08-23 15:17:58 -0500

    added comments and cleaned up some of hte code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # liri-node-app
+
+A link to the working app is here: https://drive.google.com/file/d/1-EriWi7gIfVfyxEd03Tbg7z0JlMmLxaZ/view
+
+To use the app, type one of the commands below:
+`concert-this *artist to search for*`
+`spotify-this-song *song to search for*`
+`movie-this *movie to search for*`
+`do-what-it-says *opens a file and excecutes the command in there*`
+
--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//all of the requires for the app as well as including the spotify keys
 require("dotenv").config();
 var fs = require("fs");
 var axios = require("axios");
@@ -6,6 +7,7 @@ var moment = require("moment");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+//function to get the concert info. this will ise the bootcamp api key. it will print the concert information to the console.
 function getConcertInfo(artist) {
     let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(function (response) {
@@ -21,6 +23,8 @@ function getConcertInfo(artist) {
     });
 };
 
+//function to spotify a song. tht spotify npm is called and then promised a response. the response is printed to screen. if there isn't a state associated with the city,
+// then nothing will be printed fro teh state. if there is no preview of the song, then nothing will be printed for it either.
 function getSpotifyInfo(song) {
     spotify
         .search({ type: 'track', query: song })
@@ -29,15 +33,15 @@ function getSpotifyInfo(song) {
             console.log(`You searched for ${song}. This is what I found...`);
             console.log("******************************");
             for (var i = 0; i < response.tracks.items.length; i++) {
-                var artistNamesArray=[];
+                var artistNamesArray = [];
                 var artistNamesString;
-                for (var j=0; j<response.tracks.items[i].artists.length;j++){
+                for (var j = 0; j < response.tracks.items[i].artists.length; j++) {
                     artistNamesArray.push(response.tracks.items[i].artists[j].name);
                 };
-                artistNamesString=artistNamesArray.join(" and ");
+                artistNamesString = artistNamesArray.join(" and ");
                 console.log()
                 console.log("   " + response.tracks.items[i].name + " by " + artistNamesString + " on the album " + response.tracks.items[i].album.name);
-                if ( response.tracks.items[i].preview_url!=null){
+                if (response.tracks.items[i].preview_url != null) {
                     console.log(response.tracks.items[i].preview_url);
                 }
                 console.log("")
@@ -46,20 +50,21 @@ function getSpotifyInfo(song) {
         .catch(function (err) {
             console.log(err);
         });
-        console.log("******************************");
+    console.log("******************************");
 };
 
-function getMovie(movie){
+//function to get the movie information. in the finction if no rating for the movie is found for IMDB or RT, then it will display NA for them. axios is used to get the omdb response.
+function getMovie(movie) {
     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    axios.get(queryUrl).then(function(response){
-        var IMDBRating="NA";
-        var RotttenTomatoesRating="NA";
+    axios.get(queryUrl).then(function (response) {
+        var IMDBRating = "NA";
+        var RotttenTomatoesRating = "NA";
         console.log("******************************");
-        console.log("You searched for" + movie+ ". I found this...");
+        console.log("You searched for" + movie + ". I found this...");
         console.log("******************************");
-        for(let i=0;i<response.data.Ratings.length;i++){
-            IMDBRating=response.data.Ratings[i].Source==="Internet Movie Database"?response.data.Ratings[i].Value:IMDBRating;
-            RotttenTomatoesRating=response.data.Ratings[i].Source==="Rotten Tomatoes"?response.data.Ratings[i].Value:RotttenTomatoesRating;
+        for (let i = 0; i < response.data.Ratings.length; i++) {
+            IMDBRating = response.data.Ratings[i].Source === "Internet Movie Database" ? response.data.Ratings[i].Value : IMDBRating;
+            RotttenTomatoesRating = response.data.Ratings[i].Source === "Rotten Tomatoes" ? response.data.Ratings[i].Value : RotttenTomatoesRating;
         }
         console.log(`
         Title: ${response.data.Title}
@@ -75,46 +80,49 @@ function getMovie(movie){
     });
 };
 
-function justDoIt(){
-    fs.readFile("./random.txt", "utf8", function(error, data) {
+//function to get the command from a text file and do what it says. it will call the LIRI function. 
+// there is a catch to make sure it doesn't end up in a recursive loop.
+function justDoIt() {
+    fs.readFile("./random.txt", "utf8", function (error, data) {
         if (error) {
-          return console.log(error);
+            return console.log(error);
         }
         console.log("******************************");
         console.log("The file random.txt says: " + data);
         console.log("******************************");
         console.log(" ")
         var dataArr = data.split(",");
-        if(dataArr[0]==="do-what-it-says"){
+        if (dataArr[0] === "do-what-it-says") {
             console.log("I am already doing what it ways.");
         }
-        else{
-            LIRI(dataArr[0],dataArr[1]);
+        else {
+            LIRI(dataArr[0], dataArr[1]);
         };
-
-
-      });
+    });
 };
 
-  
 
+//grabing the user command and query and join the queyr with a space between words
 const userCommand = process.argv[2];
 const userQuery = process.argv.slice(3).join(" ");
 
-function LIRI(command,query){
-switch (command) {
-    case "concert-this":
-        getConcertInfo(query);
-        return
-    case "spotify-this-song":
-        getSpotifyInfo(query===""?"The Sign":query);
-        return
-    case "movie-this":
-        getMovie(query===""?"Mr Nobody":query);
-        return
-    case "do-what-it-says":
-        justDoIt();
-        return
+//LIRI app. all of the commands will be in a switch statement.
+function LIRI(command, query) {
+    switch (command) {
+        case "concert-this":
+            getConcertInfo(query);
+            return
+        case "spotify-this-song":
+            getSpotifyInfo(query === "" ? "The Sign" : query);
+            return
+        case "movie-this":
+            getMovie(query === "" ? "Mr Nobody" : query);
+            return
+        case "do-what-it-says":
+            justDoIt();
+            return
+    };
 };
-};
-LIRI(userCommand,userQuery);
+
+//final call of the LIRI app.
+LIRI(userCommand, userQuery);
